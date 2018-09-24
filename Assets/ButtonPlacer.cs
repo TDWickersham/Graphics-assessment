e4d737de0e76aa60271e52f77c7782b39a7cbe75
TDWickersham <s179067@students.aie.edu.au>
2018-09-24 14:41:28 -0700

    small object adjustments

---
+++ Assets/ButtonPlacer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonPlacer : MonoBehaviour
+{
+    public GameObject[] placements;
+    public Material[] change;
+	
+
+    public void PlaceHouse()
+    {
+        GameObject.Instantiate<GameObject>(placements[1]);
+    }
+
+    public void PlaceTree()
+    {
+        GameObject.Instantiate<GameObject>(placements[0]);
+    }
+
+    public void PlacePath()
+    {
+        GameObject.Instantiate<GameObject>(placements[2]);
+    }
+
+    public void ChangeBrick()
+    {
+        
+    }
+
+    public void ChangeWood()
+    {
+
+    }
+
+    public void ChangeMetal()
+    {
+
+    }
+}
